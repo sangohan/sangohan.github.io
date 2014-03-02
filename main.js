26ff2bc3d9febe0b41fd208b66e08987aecdb32e
sangohan <sangohan.joe+github@gmail.com>
2014-03-02 18:19:41 +1100

    fix up some urls

--- main.js
+++ main.js
@@ -248,7 +248,7 @@ function ajax(url,callback){
 	var ajaxRequest;
 	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
 	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){}}}
-	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
+	ajaxRequest.open('GET',url,true);ajaxRequest.send(null);
 }
 
 function getElementsByClassName(node,classname) {
@@ -389,7 +389,7 @@ Game.Init=function()
 	Game.FetchCSS=function()
 	{
 		Game.Log('Loading custom stylesheet at '+Game.css+'...');
-		ajax('server.php?q=getcss|'+Game.css,Game.FetchCSSResponse);
+		ajax(Game.css,Game.FetchCSSResponse);
 	}
 	Game.FetchCSSResponse=function(response)
 	{
